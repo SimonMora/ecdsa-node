997c62d7936cd2be9849e1fa7a3ed6146fd9c39e
Simon Mora <simonmora30@gmail.com>
2024-06-07 00:22:29 +0900

    Adding functionality to verify accounts

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,53 +1,123 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { sha256 } from "ethereum-cryptography/sha256";
+import { utf8ToBytes} from "ethereum-cryptography/utils";
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
+  
+  const [canSubmit, setCanSubmit] = useState(true);
+  const [signedTransaction, setSignedTransaction] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
   async function transfer(evt) {
     evt.preventDefault();
+    console.log(signedTransaction);
 
     try {
       const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+        data: { balance }
+      } = await server.post(`send`,
+        {
+          sender: address,
+          amount: parseInt(sendAmount),
+          recipient,
+          sign: JSON.stringify(signedTransaction, (_, v) => typeof v === 'bigint' ? v.toString() : v),
+        }
+      );
       setBalance(balance);
+      setSendAmount("");
+      setRecipient("");
+      setPrivateKey("");
+      setCanSubmit(true);
     } catch (ex) {
       alert(ex.response.data.message);
     }
   }
 
+  function showModal() {
+    const modal = document.getElementById("signModal");
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    const modal = document.getElementById("signModal");
+    modal.style.display = "none";
+  }
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === 'Escape')
+      closeModal();
+  });
+
+  function signTransaction(evt) {
+    evt.preventDefault();
+
+    try {
+      const transactionDetails = {
+        sender: address,
+        amount: parseInt(sendAmount),
+        recipient,
+      };
+      const hashedTransaction = sha256(utf8ToBytes(JSON.stringify(transactionDetails)));  
+      const signedTransaction = secp256k1.sign(hashedTransaction, privateKey);
+
+      setSignedTransaction(signedTransaction.toDERHex());
+      setCanSubmit(false);
+      closeModal();
+    } catch (error) {
+      alert(error);
+      closeModal();
+    }
+    
+  }
+
   return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+    <>
+      <form className="container transfer" onSubmit={transfer}>
+        <h1>Send Transaction</h1>
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+        <label>
+          Send Amount
+          <input
+            placeholder="1, 2, 3..."
+            value={sendAmount}
+            onChange={setValue(setSendAmount)}
+          ></input>
+        </label>
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
-
-      <input type="submit" className="button" value="Transfer" />
-    </form>
+        <label>
+          Recipient
+          <input
+            placeholder="Type an address, for example: 0x2"
+            value={recipient}
+            onChange={setValue(setRecipient)}
+          ></input>
+        </label>
+        <button type="button" className="button" onClick={() => showModal()}>Sign Transaction</button>
+        <button type="submit" className="button" disabled={canSubmit}>Transfer</button>
+      </form>
+      
+      <div id="signModal" className="signModal">
+        <div className="modalContent">
+          <form onSubmit={signTransaction}>
+            <h1>Sign your Transaction</h1>
+            <input
+              placeholder="Private Key"
+              value={privateKey}
+              onChange={setValue(setPrivateKey)}
+              className="privateKeyInput"
+            ></input>
+            <input type="submit" className="button" value="Sign" disabled={!privateKey}/>
+          </form>
+        </div>
+      </div>
+    </>
+    
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const { secp256k1 }  = require("ethereum-cryptography/secp256k1");
+
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { utf8ToBytes} = require("ethereum-cryptography/utils");
+
+
 const app = express();
 const cors = require("cors");
 const port = 3042;
@@ -7,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03f2496550c707096a41f1a2471e9f0517bbfb6ad755ba7a0b1fc4b4db6204f5b1": 100,
+  "03ffa8a95004b0d57e76f0727528d510a6a35255bbdd55df8b60c61af95632911e": 50,
+  "026666b16b1dfc4530db18b3dd371ad03cf816c14ba26debc7cc243d9e667515c4": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,17 +25,30 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, sign } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  const transactionDetails = {
+    sender,
+    amount,
+    recipient,
+  };
+  const hashedMessage = sha256(utf8ToBytes(JSON.stringify(transactionDetails)));
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+  const isSigned = secp256k1.verify(sign.replaceAll('"',''), hashedMessage, sender);
+
+  if (isSigned) {
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    res.status(400).send({ message: "Sign is incorrect, please verify!" });
   }
 });
 
